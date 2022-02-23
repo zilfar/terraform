e4422b127f3ff24fe7d9619c9ca4fda7eb25efe2
ZF <zfarik@spartaglobal.com>
2022-02-23 14:46:21 +0000

    created hidden variable file to encapsulate our sensitive info

--- main.tf
+++ main.tf
@@ -6,6 +6,29 @@ provider "aws" {
     region = "eu-west-1" 
 
 # what do we want to launch
+}
 
+# init with terraform `terraform init`
+# what do we want to launch
+# automate the process of creating EC2 instance
 
+# name of the resource
+resource "aws_instance" "zilamo_tf_app" {
+
+# which AMI to use
+    ami = var.app_ami_id
+
+# what type of instance
+    instance_type = var.default_instance
+
+# name the keypair
+    key_name = var.keypair
+
+# do you want public IP
+    associate_public_ip_address = var.public_ip_boolean
+
+# what is the name of your instance
+    tags = {
+        Name = var.Name_tag
+    }
 }
---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "app_ami_id" {
+
+    default = "ami-07d8796a2b0f8d29c"
+
+}
+
+variable "default_instance" {
+
+    default = "t2.micro"
+
+}
+
+variable "keypair" {
+
+    default = "eng103a_zilamo"
+
+}
+
+variable "public_ip_boolean" {
+
+    default = true
+
+}
+
+variable "Name_tag" {
+
+    default = "eng103a_zilamo_tf_app"
+
+}
